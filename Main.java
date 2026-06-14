e6f6d0ea25aba46f937dcbb7de09d327e3b7637a
Master <andrey.markelov2003@gmail.com>
2026-06-14 20:57:01 +0500

    условные операторы комбинации

--- Main.java
+++ Main.java
@@ -5,23 +5,23 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-       int clientOS = 0;
-       if (clientOS == 0) {
+        int clientOS = 1;
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS поссылке");
         } else if (clientOS == 1) {
-           System.out.println("Установите версию приложения для Android по ссылке");
-       }
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else System.out.println("Ошибка!");
 
-        int clientDeviceYear = 2015;
+        int clientDeviceYear = 2014;
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS поссылке");
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облнгчённую версию приложения для iOS поссылке");
-        } else {
+            System.out.println("Установите облегчённую версию приложения для iOS поссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке");
-        }
+        } else System.out.println("Ошибка!");
 
         int year = 2021;
         if (year <= 1584) {
@@ -39,42 +39,41 @@ public class Main {
         if (deliveryDistance > 100) {
             System.out.println("К сожалению, на такое расстояние доставка не осуществляется");
         } else if (deliveryDistance <= 20) {
-                System.out.println("Потребуется дней на доставку: " + deliveryTime);
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60 ) {
-                System.out.println("Потребуется дней на доставку: " + 2 * deliveryTime);
-            } else {
-                System.out.println("Потребуется дней на доставку: " + 3 * deliveryTime);
-            }
+            System.out.println("Потребуется дней на доставку: " + deliveryTime);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней на доставку: " + 2 * deliveryTime);
+        } else {
+            System.out.println("Потребуется дней на доставку: " + 3 * deliveryTime);
+        }
 
         int monthNumber = 4;
         if (monthNumber > 12 || monthNumber <= 0) {
             System.out.println("Такого месяца не существует!");
-        } else { switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("Сейчас зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Сейчас весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Сейчас лето!");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Сейчас осень");
-                break;
+        } else {
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    System.out.println("Сейчас зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Сейчас весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Сейчас лето!");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Сейчас осень");
+                    break;
+            }
         }
-        }
 
 
-
-
-        }
+    }
 }
